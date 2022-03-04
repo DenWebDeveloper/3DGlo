20e1c67970f1cbc29a32bd87cb731618769ab8a4
Denis Shabanov <step-klim@mail.ru>
2022-03-04 22:32:58 +0300

    1

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
     entry: './src/index.js',
     output: {
-        fiename: 'one.js',
+        filename: 'one.js',
         path: path.resolve(__dirname, 'dist')
     }
 };
