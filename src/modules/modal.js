17674ff7434189f25340bb45471c63bc0f728b9b
Denis Shabanov <step-klim@mail.ru>
2022-03-11 01:01:38 +0300

    Сделал анимацию popup окна

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -2,7 +2,51 @@
 
 const modal = () => {
 
-    console.log("modal");
+    const modal = document.querySelector('.popup');
+    const popupContent = modal.querySelector('.popup-content');
+   
+    const buttons = document.querySelectorAll('.popup-btn');
+    const closeBtn = modal.querySelector('.popup-close');
+
+    let idInterval;
+
+    buttons.forEach(btn => {
+        btn.addEventListener('click', () => {
+            
+            modal.style.display = 'block';
+            popupAnimation();
+
+            
+        });
+    });
+
+    closeBtn.addEventListener('click', () => {
+        
+
+        modal.style.display = 'none';
+  
+
+    });
+
+    let count = 0;
+
+    const popupAnimation = () => {
+
+        count++;
+        idInterval = requestAnimationFrame(popupAnimation);
+        
+        if( count < 191){
+            popupContent.style.top = (count * 2)  - 191 + 'px';
+        }else {
+            cancelAnimationFrame(idInterval);
+        }
+        
+    };
+
+    popupAnimation();
+   
+
+
 };
 
 export default modal;
